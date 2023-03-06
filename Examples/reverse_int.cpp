a9d23686b2aa344707ecdb29000efd8cdc2aef89
Bartek Nowak Dom <barteknowak9418@gmail.com>
2023-03-06 21:46:38 +0100

    Create reverse_int.cpp

---
+++ Examples/reverse_int.cpp
@@ -0,0 +1,12 @@
+#include<iostream>;
+#include<string>;
+using namespace std; 
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        return rev == s;
+    }
+};
